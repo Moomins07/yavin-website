f742a4b7eb5e2bc30baf5c392d852c12b2013acf
David Smith <96678397+Moomins07@users.noreply.github.com>
2023-12-13 15:38:43 +0000

    completed up to 'back to top button'

--- js/script.js
+++ js/script.js
@@ -1,14 +1,22 @@
 // Add custom JavaScript here
 function userScroll() {
   const navbar = document.querySelector('.navbar');
+  const toTopBtn = document.querySelector('#to-top');
 
   window.addEventListener('scroll', () => {
     if (window.scrollY > 50) {
       navbar.classList.add('navbar-sticky');
+      toTopBtn.classList.add('show');
     } else {
       navbar.classList.remove('navbar-sticky');
+      toTopBtn.classList.remove('show');
     }
   });
+}
+
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
 
 function incrementStats() {
@@ -35,5 +43,7 @@ function incrementStats() {
   });
 }
 
+// Event Listeners
+document.querySelector('#to-top').addEventListener('click', scrollToTop);
 document.addEventListener('DOMContentLoaded', userScroll);
 document.addEventListener('DOMContentLoaded', incrementStats);
